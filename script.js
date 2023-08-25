2f768d67a8f9a955823efdd95bb652a2c01533d1
ahmetkamilates <ahmetkamil2021@gmail.com>
2023-08-25 15:13:47 +0300

    main

---
+++ script.js
@@ -0,0 +1,15 @@
+var ImgBox = document.querySelector(".imgBox");
+var ImgWrap = document.querySelector(".imgWrap");
+var originalImg = document.getElementById("originalImg");
+var line = document.getElementById("line");
+
+
+originalImg.style.width = ImgBox.offsetWidth + "px";
+
+var leftSpace = ImgBox.offsetLeft;
+
+ImgBox.onmousemove = function(e){
+   var boxWidth = (e.pageX - leftSpace) + "px";
+    ImgWrap.style.width = boxWidth;
+    line.style.left = boxWidth;
+}
